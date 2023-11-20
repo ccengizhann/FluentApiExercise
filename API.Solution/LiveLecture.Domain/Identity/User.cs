b56c021f3648842a0f254c693ed8d8edc405c06f
Cengizhan <civelekc18@itu.edu.tr>
2023-11-20 23:38:42 +0300

    [Feat]Added identity classes.

---
+++ API.Solution/LiveLecture.Domain/Identity/User.cs
@@ -0,0 +1,29 @@
+﻿using LiveLecture.Domain.Entities;
+using LiveLecture.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveLecture.Domain.Identity
+{
+    public class User : IdentityUser<Guid>, IEntityBase<Guid>, ICreatedByEntity, IModifiedByEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTimeOffset? BirthDate { get; set; }
+        public Gender Gender { get; set; }
+
+        public UserSetting UserSetting { get; set; }
+
+        public string CreatedByUserId { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public string? ModifiedByUserId { get; set; }
+        public DateTimeOffset? LastModifiedOn { get; set; }
+
+       
+    }
+}
