c8e84f4ec51ad6fecbe6d00e93ff5d651293f338
Cengizhan <civelekc18@itu.edu.tr>
2023-11-22 17:20:41 +0300

    [Feat] Added user, usersetting and ıdentitydb

---
+++ API.Solution/LiveLecture.Persistence/Contexts/YetgenIdentityContext.cs
@@ -0,0 +1,31 @@
+﻿using LiveLecture.Domain.Entities;
+using LiveLecture.Domain.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveLecture.Persistence.Contexts
+{
+    public class YetgenIdentityContext: IdentityDbContext<User,Role,Guid>
+    {
+        public YetgenIdentityContext(DbContextOptions<YetgenIdentityContext>dbContextOptions) : base(dbContextOptions) 
+        
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Ignore<Student>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
